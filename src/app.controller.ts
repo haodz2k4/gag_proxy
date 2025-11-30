161b632f8e7a4cbd76108f2f8f8503369ded684f
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-11-30 12:58:59 +0700

    init project

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import type { Request } from 'express';
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    console.log("Run here")
+    return this.appService.getHello();
+  }
+
+  @Post()
+  test(@Req() request: any): any {
+    console.log(request.body)
+    return request.body
+  }
+}
